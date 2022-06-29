acab4deba76b09e92e789ce6573d3cef04ed56ef
Agus Adi <gusadi872@gmail.com>
2022-06-30 00:20:32 +0800

    add import and export state example

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+
+const { robusta } = require('./state');
+const coffeeStock = require('./state');
+
+const makeCoffee = (types, milligram) => {
+    if (coffeeStock[types] >= milligram) {
+        console.log('kopi berhasil dibuat');
+    } else {
+        console.log('Biji kopi habis!')
+    }
+}
+
+makeCoffee('robusta', 80)
